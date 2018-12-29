fe77f81a8a612f9a1a79baada6ec829a48170069
bai <504727360@qq.com>
2018-12-29 17:08:20 +0800

    5.can see TO-DO

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 import unittest
+import time
 class NewvisitorTest(unittest.TestCase):
 	def setUp(self):
 		self.driver=webdriver.Firefox()
@@ -7,6 +8,7 @@ class NewvisitorTest(unittest.TestCase):
 		self.driver.quit()
 	def test_found_todo(self):	
 		self.driver.get('http://localhost:8000')
+		time.sleep(2)
 #公司新增了一个代办事项管理工具
 #白去首页查看，标题和头部有to-do的提示
 #他输入了第一个代办事项，test hoden
@@ -17,7 +19,7 @@ class NewvisitorTest(unittest.TestCase):
 #完成输入后，页面刷新了输入框；同时出现了代办事项列表更新为2个
 #白确定可以记录个人的唯一url
 #白重新打开这个网站，代办信息还在
-		self.assertIn( 'to-do',self.driver.title)
+		self.assertIn( 'To-Do',self.driver.title)
 		self.fail('Finish the test!')
 if __name__=='__main__':
 	unittest.main(warnings='ignore')
--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,17 @@
 from django.test import TestCase
+from django.urls import resolve
+from lists.views import home_page
+from django.http import HttpRequest
 
 # Create your tests here.
-class smokeTest(TestCase):
-	def test_add(self):
-		self.assertEqual(1+1,3)
+class HomePageTest(TestCase):
+	def test_resolve_homepage_rootview(self):
+		found=resolve('/')
+		self.assertEqual(found.func,home_page)
+	def test_home_page_return_correct_html(self):
+		request=HttpRequest()
+		response=home_page(request)
+		html=response.content.decode('utf-8')
+		self.assertTrue(html.startswith('<html>'))
+		self.assertIn('<title>To-Do Lists</title>',html)
+		self.assertTrue(html.endswith('</html>'))
