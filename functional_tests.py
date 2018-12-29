31205ffbd502b5bcbe48d4cd10d44f23a34736cc
bai <504727360@qq.com>
2018-12-29 17:35:45 +0800

    6.new functest code

--- functional_tests.py
+++ functional_tests.py
@@ -11,15 +11,26 @@ class NewvisitorTest(unittest.TestCase):
 		time.sleep(2)
 #公司新增了一个代办事项管理工具
 #白去首页查看，标题和头部有to-do的提示
+		self.assertIn( 'To-Do',self.driver.title)
+		headtext=self.driver.find_element_by_tag_name('h1').text
+		self.assertIn('To-Do',headtext)
 #他输入了第一个代办事项，test hoden
+		input=self.driver.find_element_by_id("id_new_item");L C
+		self.assertEqual(input.get_attribute('placeholder'),"Enter a to-do item")
+		input.send_keys('test hoden')
 #完成输入后，enter页面刷新了
+		input.send_keys(Keys.ENTER)
+		time.sleep(1)
 #这时页面出现了一条代办事项；1. test hoden；
+		table=self.driver.find_element_by_id('id_list_table')
+		rows=table.find_elements_by_tag_name('tr')
+		self.assertTrue(row.text=='1.test hoden' for row in rows)
 #页面此时又刷新了一个新的输入框，可以输入新的代办事项
 #白输入了代办事项：2.retry bugs
 #完成输入后，页面刷新了输入框；同时出现了代办事项列表更新为2个
 #白确定可以记录个人的唯一url
 #白重新打开这个网站，代办信息还在
-		self.assertIn( 'To-Do',self.driver.title)
+		
 		self.fail('Finish the test!')
 if __name__=='__main__':
 	unittest.main(warnings='ignore')
